57615286a37c95585a75530316a1e1926be615ab
kelvynking <kelvyn.m.king@gmail.com>
2022-01-30 21:23:43 +1100

    Saved form data on firebase.

--- src/components/EnterStats.js
+++ src/components/EnterStats.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { useRef } from "react";
+import { useHistory } from "react-router-dom";
 
 function EnterStats() {
-    return (
-        <div>
-            This is the enter stats page.
-        </div>
-    )
+  const dateInputRef = useRef();
+  const history = useHistory();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const dateEntered = dateInputRef.current.value;
+
+    const addCovidData = {
+      testDate: dateEntered,
+    };
+
+    fetch(
+      "https://covid-tracker-ad13d-default-rtdb.asia-southeast1.firebasedatabase.app/stats.json",
+      {
+        method: "POST",
+        body: JSON.stringify(addCovidData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/viewstats");
+    });
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label>
+        Date of test:
+        <input type="text" name="testdate" ref={dateInputRef}></input>
+      </label>
+      <button type="submit" value="Submit">
+        Submit
+      </button>
+    </form>
+  );
 }
 
 export default EnterStats;
