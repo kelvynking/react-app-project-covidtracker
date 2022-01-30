f48b008ede308512df115da22003c4ebdcf62d75
kelvynking <kelvyn.m.king@gmail.com>
2022-01-30 16:38:47 +1100

    Added 'Navigation' component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import EnterStats from './components/EnterStats';
+import ViewStats from './components/ViewStats';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Covidtracker</h1>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Navigation />
+          <Route exact path="/" component={Home} /> 
+          <Route exact path="/enterstats" component={EnterStats} />
+          <Route exact path="/viewstats" component={ViewStats} />
+          <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
