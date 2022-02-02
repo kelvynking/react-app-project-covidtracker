d6907b17e23250bb5bd7eb3b53557e947cbb5c85
kelvynking <kelvyn.m.king@gmail.com>
2022-02-02 19:38:32 +1100

    Added 'useState' in 'EnterStats.js' to store radiobox values.

--- src/components/EnterStats.js
+++ src/components/EnterStats.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { useRef } from "react";
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 
 function EnterStats() {
   const dateInputRef = useRef();
+  const symptomsInputRef = useRef();
   const history = useHistory();
+
+  const [radioCheck, setRadioCheck] = useState("positive");
+
+  const handleChange = (event) => {
+    setRadioCheck(event.target.value);
+  };
 
   const submitHandler = (event) => {
     event.preventDefault();
 
     const dateEntered = dateInputRef.current.value;
+    const symptomsEntered = symptomsInputRef.current.value;
 
     const addCovidData = {
       testDate: dateEntered,
+      symptoms: symptomsEntered,
+      result: radioCheck,
     };
 
     fetch(
@@ -31,11 +42,63 @@ function EnterStats() {
 
   return (
     <form onSubmit={submitHandler}>
-      <label>
-        Date of test:
-        <input type="text" name="testdate" ref={dateInputRef}></input>
-      </label>
-      <button type="submit" value="Submit">
+      <div className="row mb-3">
+        <label for="inputDate" className="col-sm-2 col-form-label">
+          Date of test:
+        </label>
+        <div className="col-sm-10">
+          <input type="test" className="testdate" ref={dateInputRef}></input>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <label for="symptoms" className="col-sm-2 col-form-label">
+          Symptoms:
+        </label>
+        <div className="col-sm-10">
+          <textarea
+            type="text"
+            className="symptoms"
+            ref={symptomsInputRef}
+          ></textarea>
+        </div>
+      </div>
+      <fieldset className="row mb-3">
+        <legend className="col-form-label col-sm-2 pt-0">
+          PCR/RAT Results:
+        </legend>
+        <div className="col-sm-10">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="gridRadios"
+              id="gridRadios1"
+              value="positive"
+              checked={radioCheck === "positive"}
+              onChange={handleChange}
+            ></input>
+            <label className="form-check-label" for="gridRadios1">
+              Positive
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="gridRadios"
+              id="gridRadios2"
+              value="negative"
+              checked={radioCheck === "negative"}
+              onChange={handleChange}
+            ></input>
+            <label className="form-check-label" for="gridRadios2">
+              Negative
+            </label>
+          </div>
+        </div>
+      </fieldset>
+
+      <button type="submit" className="btn btn-primary">
         Submit
       </button>
     </form>
--- src/components/ViewStats.js
+++ src/components/ViewStats.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function ViewStats() {
-    return (
-        <div>
-            This is the view stats page.
-        </div>
-    )
+  return <div>This is the view stats page.</div>;
 }
 
 export default ViewStats;
