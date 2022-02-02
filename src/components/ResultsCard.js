7cb0c31623c95e64a7bd9e4cdeaa70e9f56a3b19
kelvynking <kelvyn.m.king@gmail.com>
2022-02-02 21:05:30 +1100

    Fetch results data from firebase in 'ViewStats.js' and display using 'ResultsCard' component.

---
+++ src/components/ResultsCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function ResultsCard(props) {
+  return (
+    <div className="card w-75 m-auto mb-3">
+      <div className="card-header">PCR/RAT Test Result</div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">
+          <span className="fw-bold">Test Date: </span>
+          {props.testDate}
+        </li>
+        <li className="list-group-item">
+          <span className="fw-bold">Symptoms: </span>
+          {props.symptoms}
+        </li>
+        <li className="list-group-item">
+          <span className="fw-bold">Result: </span>
+          {props.result}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ResultsCard;
--- src/components/ViewStats.js
+++ src/components/ViewStats.js
@@ -1,7 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import ResultsCard from "./ResultsCard";
 
 function ViewStats() {
-  return <div>This is the view stats page.</div>;
+  const [getStats, setGetStats] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://covid-tracker-ad13d-default-rtdb.asia-southeast1.firebasedatabase.app/stats.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const resultsArray = [];
+
+        for (const key in data) {
+          const resultObj = {
+            id: key,
+            ...data[key],
+          };
+          resultsArray.push(resultObj);
+        }
+        setGetStats(resultsArray);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Fetching your results.</div>;
+  }
+  return (
+    <div className="container p-3">
+      {getStats.map((result) => (
+        <ResultsCard
+          key={result.id}
+          result={result.result}
+          symptoms={result.symptoms}
+          testDate={result.testDate}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default ViewStats;
